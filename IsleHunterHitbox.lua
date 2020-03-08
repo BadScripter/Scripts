edb306d431532a4e812abcadeb54ac04e80be083
BadScripter <36619241+BadScripter@users.noreply.github.com>
2020-03-08 16:11:50 -0700

    Create IsleHunterHitbox.lua

---
+++ IsleHunterHitbox.lua
@@ -0,0 +1,27 @@
+-- Made by TrashScripterF, GUI Lib by Wally
+
+local library = loadstring(game:HttpGet("https://pastebin.com/raw/7Z6TzFnv", true))()
+local hb = library:CreateWindow({
+  text = "Hitbox"
+})
+
+local credits = library:CreateWindow({text='Credits'})
+credits:AddLabel("Scripter: TrashScripterF\nGUI Lib: Wally")
+
+
+hb:AddBox("Size", function(object, focus)
+	if focus then
+    	for i, v in pairs(workspace.AIHunter:GetChildren()) do
+        if v:FindFirstChild("HumanoidRootPart") and v:FindFirstChild("UpperTorso") then
+          v.HumanoidRootPart.Size = Vector3.new(tonumber(object.Text),tonumber(object.Text),tonumber(object.Text))
+          v.Character.UpperTorso.Size = Vector3.new(tonumber(object.Text),tonumber(object.Text),tonumber(object.Text))
+        end
+		end
+	end
+end)
+
+game.StarterGui:SetCore("SendNotification", {
+	Title = "GUI Loaded",
+	Text = "Join my discord for more scripts: https://discord.gg/czXkbCS",
+	Duration = 5,
+})
