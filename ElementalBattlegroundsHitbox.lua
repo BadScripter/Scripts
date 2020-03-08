0b274148633ef79aed3d185a5ca83317cad53997
BadScripter <36619241+BadScripter@users.noreply.github.com>
2020-03-08 01:11:02 -0800

    Create ElementalBattlegroundsHitbox.lua

---
+++ ElementalBattlegroundsHitbox.lua
@@ -0,0 +1,27 @@
+-- Made by TrashScripterF
+
+local library = loadstring(game:HttpGet("https://pastebin.com/raw/7Z6TzFnv", true))()
+local hb = library:CreateWindow({
+  text = "Hitbox"
+})
+
+local credits = library:CreateWindow({text='Credits'})
+credits:AddLabel("Scripter: TrashScripterF\nGUI Lib: Wally")
+
+
+hb:AddBox("Size", function(object, focus)
+	if focus then
+    	for i, v in pairs(game.Players:GetPlayers()) do 
+			if v.Character and v ~= game.Players.LocalPlayer then 
+				v.Character.HumanoidRootPart.Size = Vector3.new(tonumber(object.Text),tonumber(object.Text),tonumber(object.Text))
+				v.Character.UpperTorso.Size = Vector3.new(tonumber(object.Text),tonumber(object.Text),tonumber(object.Text))
+			end
+		end
+	end
+end)
+
+game.StarterGui:SetCore("SendNotification", {
+	Title = "GUI Loaded",
+	Text = "Join my discord for more scripts: https://discord.gg/czXkbCS",
+	Duration = 5,
+})
