0807333f83240f8c6e87977f0e57f08906d3ace1
BadScripter <36619241+BadScripter@users.noreply.github.com>
2020-03-07 20:32:10 -0800

    Update LostESP.lua

--- LostESP.lua
+++ LostESP.lua
@@ -9,19 +9,19 @@ game.StarterGui:SetCore("SendNotification", {
 while wait() do
   for i, v in pairs(workspace.ScavZones:GetDescendants()) do
     if v:FindFirstChild("Head") then
-      if not v.Head:FindFirstChild("BillboardGui") then
-        local bill = Instance.new("BillboardGui", v.Head)
+        local bill = Instance.new("BillboardGui")
+	bill.Parent = v.Head
         bill.AlwaysOnTop = true
         bill.LightInfluence = 1
         bill.Size = UDim2.new(0, 50, 0, 50)
         bill.StudsOffset = Vector3.new(0, 3.5, 0)
-        local role = Instance.new("TextLabel", bill)
-        role.Text = v.Parent.Parent.Parent.Name
+        local role = Instance.new("TextLabel")
+	role.Parent = bill
+        role.Text = v.Name
         role.TextColor3 = Color3.new(1,0,0)
         role.TextScaled = true
         role.BackgroundTransparency = 1
         role.Size = UDim2.new(0,50,0,50)
-       end
     end	
   end
 end
