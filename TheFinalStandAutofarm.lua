33e9f301af0fec43e7cabf63921709cb346f54b3
BadScripter <36619241+BadScripter@users.noreply.github.com>
2020-03-07 18:15:47 -0800

    Update TheFinalStandAutofarm.lua

--- TheFinalStandAutofarm.lua
+++ TheFinalStandAutofarm.lua
@@ -41,7 +41,7 @@ game:GetService("UserInputService").InputEnded:Connect(function(i,g)
 	if i.KeyCode == Enum.KeyCode[_G.Toggle:upper()] then
 		on = not on
 		game.StarterGui:SetCore("SendNotification", {
-			Title = "Autofarm is now on!",
+			Title = "Autofarm is now set to "..on.."!",
 			Text = "Join my discord for more scripts: https://discord.gg/czXkbCS",
 			Duration = 1.5,
 		})
