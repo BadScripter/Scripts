c9178f9afdcca38d19ec1bd59e1f5fb975688603
BadScripter <36619241+BadScripter@users.noreply.github.com>
2020-04-01 18:32:01 -0700

    Update BloxnoHero2AutoFarm.lua

--- BloxnoHero2AutoFarm.lua
+++ BloxnoHero2AutoFarm.lua
@@ -1,7 +1,7 @@
 -- Made by TrashScripterF, GUI Lib by Wally
 
 _G.set = false
-local library = loadstring(game:HttpGet("https://pastebin.com/raw/7Z6TzFnv", true))()
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/BadScripter/Scripts/master/WallyGUILibV1.lua", true))()
 local Auto = library:CreateWindow({
   text = "Blox no Hero 2 Autofarm"
 })
