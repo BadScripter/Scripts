3588f5dbee52c675e9162cb414a83918bc6d4896
BadScripter <36619241+BadScripter@users.noreply.github.com>
2020-05-07 11:51:50 -0700

    Create VolleyballInfStam.lua

---
+++ VolleyballInfStam.lua
@@ -0,0 +1,16 @@
+-- Made by TrashScripterF
+
+assert(getsenv, "exploit not supported!")
+assert(debug.setupvalue,"exploit not supported!")
+
+for i, v in pairs(getsenv(game.Players.LocalPlayer.Character.CharacterScript)) do
+    if i == "handleAction" then
+        debug.setupvalue(v,18,math.huge)
+    end
+end
+
+game.StarterGui:SetCore("SendNotification", {
+	Title = "Inf Stam Loaded!",
+	Text = "Join my discord for more scripts: https://discord.gg/czXkbCS",
+	Duration = 5,
+})
